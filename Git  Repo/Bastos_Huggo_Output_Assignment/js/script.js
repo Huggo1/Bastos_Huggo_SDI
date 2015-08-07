77a94b9d528a3f0cc62751d645e1ef1bbfc008ea
Huggo1 <huggobastos@huggos-mbp.attlocal.net>
2015-08-06 21:55:58 -0400

    Console
    Here I have already declared all the variables and am now consoling
    them out.

--- Git  Repo/Bastos_Huggo_Output_Assignment/js/script.js
+++ Git  Repo/Bastos_Huggo_Output_Assignment/js/script.js
@@ -12,5 +12,10 @@ var isCompetitive = true;   //Boolean Variable
 
 var leagueSummonerName = "Summano"; // String
 
+console.log("Hi my name is Huggo and my height is 5foot 10inches");
+console.log("\nHere are two things about me. Am I a gamer?");
+console.log(isGamer);
+console.log("\nAm I competitive in nature?");
+console.log(isCompetitive);
 
 
