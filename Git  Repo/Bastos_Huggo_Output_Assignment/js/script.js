e8e1dd06f25dff895885f1a07b214a2bc5a9d607
Huggo1 <huggobastos@huggos-mbp.attlocal.net>
2015-08-06 21:46:21 -0400

    Variables

---
+++ Git  Repo/Bastos_Huggo_Output_Assignment/js/script.js
@@ -0,0 +1,13 @@
+/**
+ * Created by huggobastos on 8/6/15.
+ */
+// Output Assignment
+
+// This is where I'll be setting up the variables
+var height;
+height = 178;               // Number Variable
+
+
+
+
+
