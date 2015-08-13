ec792e1c02240661ec1b2e7b420213f693da05c9
Huggo1 <huggobastos@Huggos-MacBook-Pro.local>
2015-08-13 13:34:02 -0400

    Added Multiply

--- Git  Repo/Bastos_Huggo_Expressions_Assignment/js/Bastos_Huggo_Expressions_Assignment.js
+++ Git  Repo/Bastos_Huggo_Expressions_Assignment/js/Bastos_Huggo_Expressions_Assignment.js
@@ -22,18 +22,25 @@ thirdNumber = prompt("Please enter in a third number.");
 fourthNumber = prompt("Please enter in a fourth number.");
 fifthNumber = prompt("Please enter in a fifth number.");
 
+// This determines the average of the numbers the user enters by adding them all together then dividing by the total.
 var average = (Number(firstNumber) + Number(secondNumber) + Number(thirdNumber) +
     Number(fourthNumber) + Number(fifthNumber)) / 5;
 
+// The sum works by adding all of the numbers together.
 var sum = (Number(firstNumber) + Number(secondNumber) + Number(thirdNumber) +
 Number(fourthNumber) + Number(fifthNumber));
+
+//
+var multiplied = (Number(firstNumber) * Number(secondNumber) * Number(thirdNumber) *
+Number(fourthNumber) * Number(fifthNumber));
 
 //outputs
 console.log("These are the calculations of the numbers you entered.");
 console.log("Average:" + average);
 console.log("Sum:" + sum);
+console.log("All your numbers multiplied together = " + multiplied);
 
 // I tried different inputs like all 5 or all 4 or even a variety of numbers.
 // The system always gave me the correct average.
 // A specific example is I tried 10/20/30/40/50 and I got 30 as the average.
-// The
+// The sum variable was added and now works.
