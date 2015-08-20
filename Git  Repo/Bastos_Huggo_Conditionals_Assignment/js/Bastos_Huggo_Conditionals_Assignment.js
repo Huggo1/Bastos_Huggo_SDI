eb90049c8472481fb5ac3b2ee8df8ed12a062e07
Huggo1 <huggobastos@Huggos-MacBook-Pro.local>
2015-08-20 19:10:57 -0400

    Final Commit
    Added all of the comments and made sure everything was working properly.

--- Git  Repo/Bastos_Huggo_Conditionals_Assignment/js/Bastos_Huggo_Conditionals_Assignment.js
+++ Git  Repo/Bastos_Huggo_Conditionals_Assignment/js/Bastos_Huggo_Conditionals_Assignment.js
@@ -12,17 +12,16 @@ var pennies; // Amount of pennies
 var nickles; // Amount of nickles
 var dimes; // Amount of dimes
 var quarters; // Amount of quarters
-var totalCash; // Total amount of money user has in coins.
-totalCash = (pennies *.01) + (nickles *.05) + (dimes *.10) + (quarters *.25); // This is how the calculator sets that up.
+var moneyYouKeep; // Amount you get back depending on amount you deposit.
 
 pennies = Number(prompt("Please tell us how many pennies you have. Our machine can only count up to 1,000 at a time"));
-// Taking in user prompt to determine their amount of pennies.
+// Taking in user prompt to determine their amount of pennies and converts it to number.
 nickles = Number(prompt("Please tell us how many nickles you have. Our machine can only count up to 1,000 at a time"));
-// Taking in user prompt to determine their amount of nickles.
+// Taking in user prompt to determine their amount of nickles and converts it to number.
 dimes = Number(prompt("Please tell us how many dimes you have. Our machine can only count up to 1,000 at a time"));
-// Taking in user prompt to determine their amount of dimes.
+// Taking in user prompt to determine their amount of dimes and converts it to number.
 quarters = Number(prompt("Please tell us how many quarters you have. Our machine can only count up to 1,000 at a time"));
-// Taking in user prompt to determine their amount of quarters.
+// Taking in user prompt to determine their amount of quarters and converts it to number.
 
 // Set up a do while loop to determine if there is an error the user will have to re-enter in their data.
 do
@@ -68,9 +67,18 @@ pennies < 0 || nickles < 0 || dimes < 0 || quarters < 0 ||                    //
 pennies == "" || nickles == "" || dimes == ""|| quarters == "" ||             // Checks to make sure there is no blank.
 isNaN(pennies) || isNaN(nickles) || isNaN(dimes)|| isNaN(quarters));          // Makes sure user types in a number.
 
-console.log("Your total amount of money is " + totalCash);
+var totalCash; // Total amount of money user has in coins.
+totalCash = ((pennies * 1) + (nickles * 5) + (dimes * 10) + (quarters * 25)) / 100; // This is how the calculator sets that up.
 
+console.log("Your total amount of money is " + totalCash); // This tells you the total amount of money you deposited.
 
+moneyYouKeep = (totalCash < 10) ? totalCash * .9 : totalCash; // This sets up how much you keep depending on how much you deposited.
+
+console.log("You are keeping " + moneyYouKeep); // This displays how much you are keeping.
+
+// I tested 1 for everything and got .41 which is 41 cents and is correct.
+// It also worked correctly and told me what I was keeping and if it was under 10 it deducted the 10%.
+// It also worked that if you got exactly $10 you would keep the $10.
 
 
 
