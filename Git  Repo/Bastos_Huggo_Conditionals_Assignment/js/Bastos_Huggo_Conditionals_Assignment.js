6364c21a35427051c01e877ec13979886f15aa0d
Huggo1 <huggobastos@Huggos-MacBook-Pro.local>
2015-08-20 17:03:33 -0400

    Set up Conditions for first variable
    Still trying to find out how I do input validation to make sure the
    user enters a number. So far I am using a do while loop to make sure
    they enter between two certain numbers.

--- Git  Repo/Bastos_Huggo_Conditionals_Assignment/js/Bastos_Huggo_Conditionals_Assignment.js
+++ Git  Repo/Bastos_Huggo_Conditionals_Assignment/js/Bastos_Huggo_Conditionals_Assignment.js
@@ -12,6 +12,31 @@ var dimes;
 var quarters;
 
 
+pennies = prompt("Please tell us how many pennies you have. Our machine can only count up to 1,000 at a time");
+
+if (pennies > 1000)
+{
+ do
+  pennies = prompt("Please re-enter the amount of pennies. We cannot count that high.\n" +
+      "Our machine can only count up to 1,000 at a time");
+ while (pennies > 1000);
+
+}
+
+else if (pennies < 0)
+{
+ do
+  pennies = prompt("You cannot have negative pennies. Please re-enter the amount. \n" +
+      "Our machine can only count up to 1,000 at a time");
+ while (pennies < 0);
+}
+
+else (pennies <= 1000 && pennies > 0)
+{
+ pennies = prompt("Please enter in an actual number.")
+}
+
+
 
 
 
