537a83d5e0bdf424dbd42f544bf22a002508c8b3
Huggo1 <huggobastos@Huggos-MacBook-Pro.local>
2015-08-26 18:34:55 -0400

    Set up Round and other variables

--- Git  Repo/Bastos_Huggo_Functions_Assignments/js/Bastos_Huggo_Functions_Assignment.js
+++ Git  Repo/Bastos_Huggo_Functions_Assignments/js/Bastos_Huggo_Functions_Assignment.js
@@ -4,3 +4,56 @@ SDI
 Functions
 */
 
+var lottery;
+var randomNum;
+var powerballNum;
+
+lottery = prompt("Would you like to get numbers for the Florida lottery or the Powerball lottery. " +
+    "\n \n Enter \"Florida\" for Florida and \"Powerball\" for Powerball");
+
+function chooseLottery(lotterySelection)
+{
+
+ while (lotterySelection != "Florida" || lotterySelection != "Powerball")
+ {
+
+     if (lotterySelection === "Florida")
+     {
+         console.log("You have chosen numbers for the Florida lottery.");
+         break;
+     }
+
+     else if (lotterySelection === "Powerball")
+     {
+         console.log("You have chosen numbers for the Powerball");
+         break;
+     }
+
+     lotterySelection = prompt("Please enter in \"Florida\" or \"Powerball\"");
+ }
+
+return lotterySelection;
+
+}
+
+function lotteryRoll(max, min, num)
+{
+    var randomNums = [];
+
+        for (var i = 0; i < num; i++)
+        {
+            randomNums[i] = Math.random() * (max - min) + min;
+            randomNums[i] = Math.round(randomNums[i]);
+
+
+        }
+
+
+    return randomNums;
+}
+
+
+lottery = chooseLottery(lottery);
+powerballNum = Math.random() * (35-1) + 1;
+powerballNum = Math.round(powerballNum);
+
